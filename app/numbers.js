6f79003b7107078380380ef7260e0b160381cd06
Katerina Panchenko <tetsugray@gmail.com>
2021-11-13 14:33:08 +0300

    numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & num >> bit - 1;
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    var strArr = [];
+    for (var i = 7; i > -1; i--) {
+      strArr.push(num & 1 << i ? 1 : 0);
+    }
+    return strArr.join('');
   },
 
   multiply: function(a, b) {
-
+    var multiplier = 1e5;
+    var equal = (a * multiplier) * (b * multiplier) / (multiplier * multiplier);
+    return equal;
   }
 };
