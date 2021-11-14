69e35581794c164661b919bf9cd47eceabfc4bf5
Katerina Panchenko <tetsugray@gmail.com>
2021-11-14 15:58:49 +0300

    strings

--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var re = /(\w)\1+/g;
+    return str.replace(re, '$1'.repeat(amount));
   },
 
   wordWrap: function(str, cols) {
+    var strArr = [];
+    strArr = str.split(' ');
+    var formatedStr = '';
+    var formatedStr = strArr[0];
 
+    for (var i = 1; i < strArr.length; i++) {
+      if (strArr[i].length > cols) {
+        formatedStr = formatedStr + '\n' + strArr[i];
+      } else {
+        if (formatedStr.length + strArr[i].length > cols) {
+          formatedStr = formatedStr + '\n' + strArr[i];
+        } else {
+          formatedStr = formatedStr + ' ' + strArr[i];
+        }
+      }
+    }
+
+    return formatedStr;
   },
 
   reverseString: function(str) {
-
+    var strArr = str.split('');
+    return strArr.reverse().join('');
   }
 };
