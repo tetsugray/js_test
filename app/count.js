b1e78cf94cb3763fce72d4d7a2364f136eda8d5d
Katerina Panchenko <tetsugray@gmail.com>
2022-01-15 02:23:45 +0300

    count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var timeout;
 
+    function counter() {
+      if (start <= end) {
+        console.log(start);
+        timeout = setTimeout(counter, 100);
+        start++;
+      }
+    }
+
+    counter();
+
+    return {
+      cancel: function() {
+        clearTimeout(timeout);
+      }
+    };
   }
 };
